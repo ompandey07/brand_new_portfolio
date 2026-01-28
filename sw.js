3d777e297c19e3c5e5fd5bd31c858ef2a0bb1808
ompandey07 <iamompadney.it@gmail.com>
2026-01-28 12:18:24 +0545

    Aded a Service Worker

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'om-pandey-portfolio-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icons/android-chrome-192x192.png',
+  './icons/android-chrome-512x512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-32x32.png',
+  './icons/favicon-16x16.png',
+  './icons/favicon.ico'
+];
+
+// Install event - caching files
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - cleanup old caches
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList => 
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve cached content if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
